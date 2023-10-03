bb4a8786728e229b068cee937ec3cee9f22b3c25
YAMADA <c0a22143c0@edu.teu.ac.jp>
2023-10-03 15:32:53 +0900

    練習問題4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -12,14 +12,27 @@ delta = {  # 練習３：移動量辞書
     pg.K_RIGHT: (+5, 0),
     }
 
+
+def check_bound(obj_rct: pg.Rect):
+    """
+    引数：こうかとんRectかばくだんRect
+    戻り値：タプル（横方向判定結果，縦方向判定結果）
+    画面内ならTrue，画面外ならFalse
+    """
+    yoko, tate = True, True
+    if obj_rct.left < 0 or WIDTH < obj_rct.right: # 横方向判定
+        yoko = False
+    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom: # 縦方向判定
+        tate = False
+    return yoko,tate
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     ####こうかとん####
     kk_img = pg.image.load("ex02/fig/3.png")
-    kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_img = pg.transform.rotozoom(kk_img, 0, 10.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = (900, 400)  # 練習３：こうかとんの初期座標を設定する
     
@@ -32,7 +45,6 @@ def main():
     vx, vy = +5, +5 #練習２　爆弾の移動
     
     clock = pg.time.Clock()
-     
     tmr = 0
     while True:
         for event in pg.event.get():
@@ -49,10 +61,17 @@ def main():
                 sum_mv[0] += mv[0]  # 練習３：横方向の合計移動量
                 sum_mv[1] += mv[1]  # 練習３：縦方向の合計移動量
         kk_rct.move_ip(sum_mv[0], sum_mv[1])  # 練習３：移動させる
+        if check_bound(kk_rct) != (True, True):  # 練習４：はみだし判定
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)  # 練習３：移動後の座標に表示させる
         
         ####爆弾####
         bomb_rct.move_ip(vx,vy) #練習問題２：爆弾を移動させる
+        yoko,tate = check_bound(bomb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         screen.blit(bomb, bomb_rct)#表示と座標の設定
         
         
